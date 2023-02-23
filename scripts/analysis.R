dc35783069d23ee2152e450e1c528f850fa56bf0
Vidya Kamath Pailodi <vidyak2@chackoge-serv01.cs.illinois.edu>
2023-02-22 22:46:44 -0600

    fix the syntax errors

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -6,7 +6,7 @@ library(data.table)
 args = commandArgs(trailingOnly=TRUE)
 
 if (length(args)==0) {
-  stop("At least three arguments must be supplied: parent_network_cleaned.tsv","user-specified-analyis.csv", "<file_1.tsv" call.=FALSE)
+  stop("At least three arguments must be supplied: parent_network_cleaned.tsv user-specified-analysis.csv file_1.tsv", call.=FALSE)
 } else if (length(args)==3) {
   print("OK 3 params supplied")
 }
@@ -17,7 +17,7 @@ nc_denom <- length(union(source$V1,source$V2))
 
 # list of all tsv files to analyse
 algo_res <- tail(args, -2)
-algo_list_res =  <-list()
+algo_list_res <- list()
 for (i in 1:length(algo_res)) {
     algo_list_res[[i]] <- fread(algo_res[i])
  }
@@ -26,5 +26,5 @@ names(algo_list_res) <- algo_res
 
 t <- lapply(algo_list_res, FUN=function(x) x[,.N,by='V2'][N>10,.(cc=length(N),nc=sum(N)/nc_denom,count=sum(N),min=min(N),med=median(N),max=max(N))])
 t <- cbind(names(t),rbindlist(t))[,.(Rx=V1,clus_ct=cc,node_cov=nc,node_count=count,min,med,max)]
-write.table(t, file=args[2], sep=",", col.names=F, row.names=F)
+write.table(t, file=args[2], sep=",", col.names=TRUE, row.names=F)
 
