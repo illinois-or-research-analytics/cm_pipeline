5e4c8742cce57552b9ef081fd604e1d31ecddc51
Vidya Kamath <pvidyakamath@gmail.com>
2023-04-05 10:24:25 -0500

    add validations

--- source/default_config.py
+++ source/default_config.py
@@ -23,7 +23,7 @@ class DefaultConfig(object):
             self.execution_info_csv = os.path.join(
                 self.output_dir, 'execution_time.csv'
                 )
-            self.existing_ip_json = default_section[EXISTING_OP_JSON]
+            self.existing_ip_json = default_section.get(EXISTING_OP_JSON, {})
             self.existing_ip_dict = self.get_existing_op_file_paths()
         except KeyError:
             raise Exception(
@@ -33,9 +33,34 @@ class DefaultConfig(object):
                 )
 
     def get_existing_op_file_paths(self):
-        with open(self.existing_ip_json, 'r') as fp:
-            existing_ip_dict = json.load(fp)
+        existing_ip_dict = {}
+        if self.existing_ip_json:
+            with open(self.existing_ip_json, 'r') as fp:
+                existing_ip_dict = json.load(fp)
+            self._validate_config(existing_ip_dict)
         return existing_ip_dict
+
+    def _validate_config(self, existing_ip_dict):
+        res_vals = existing_ip_dict[CLUSTERED_NW_FILES].keys()
+        res_diff = set(res_vals).symmetric_difference(set(self.resolutions))
+        if res_diff:
+            message = f"The resolution values in the config file and json file " \
+                      f"do not match. config file: {self.resolutions}, " \
+                      f"json file: {list(res_vals)}"
+            raise Exception(message)
+        else:
+            for res in res_vals:
+                n_iters = existing_ip_dict[CLUSTERED_NW_FILES][res].keys()
+                n_iter_diff = set(n_iters).symmetric_difference(
+                    set(self.n_iterations)
+                    )
+                if n_iter_diff:
+                    message = f"The number_of_iterations values in the config " \
+                              f"file and json file " \
+                              f"do not match for resolution {res}" \
+                              f" config file: {self.n_iterations}," \
+                              f" json file:  {list(n_iters)}"
+                    raise Exception(message)
 
     def _create_output_dir_with_time_stamp(self):
         output_dir_name = f'{self.network_name}-cm-{self.cm_version}-pp-output-{self.timestamp}'
