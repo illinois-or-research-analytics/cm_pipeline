586ebf1a5135ec46aff7bf4c28c1583e199fa8e9
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-10-02 16:18:59 -0500

    removed shell tests

--- tests/test1.sh
+++
@@ -1,8 +0,0 @@
-python3 ../cm.py \
-        -i /shared/rsquared/cen_cleaned.tsv \
-        -c leiden \
-        -d working_dir_test1 \
-        -g 0.01 \
-        -t 1log10 \
-        -e ~/cen_leiden.01_nontree_n10_clusters.tsv \
-        -o cen_leiden.01_nontree_n10_clusters_cm.txt
--- tests/test2.sh
+++
@@ -1,8 +0,0 @@
-python3 ../cm.py \
-        --quiet \
-        -i /data3/chackoge/modularity_clustering/cen_cleaned.tsv \
-        -c leiden \
-        -g 0.0001 \
-        -t 1log10 \
-        -e /data3/vikramr2/benchmarks/S3.2_cm_ready_cen_leiden.0.0001_i2_nontree_n10.tsv \
-        -o cen_leiden.01_nontree_n10_clusters_cm.txt
